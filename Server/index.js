99da97c90f830948ccec08fe6fdd26c0882948c6
rahiblaghari <53871954+rahiblaghari@users.noreply.github.com>
2022-07-02 01:08:31 -0500

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -21,7 +21,7 @@ app.all("/", function(req, res, next) {
 app.use('/login', loginRoutes)
 app.use('/validateSignIn', SignInRoutes)
 
-const CONNECTION_URL = "mongodb+srv://Rahib:qtCpWxyxt4UPs8hE@sportalcv98.ui5cv.mongodb.net/?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://<your server username>:<your server password>@sportalcv98.ui5cv.mongodb.net/?retryWrites=true&w=majority"
 
 const PORT = process.env.PORT || 5000
 mongoose.connect(CONNECTION_URL).then(app.listen(PORT, ()=>{console.log('Server running on Port ' + PORT)})).catch((error)=>console.log(error.message))
